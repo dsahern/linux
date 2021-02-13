db8dea747be8144efe579d3a346650d44a48693d
Alexander Lobakin <alobakin@pm.me>
2021-02-13 14:11:11 +0000

    skbuff: simplify kmalloc_reserve()
    Eversince the introduction of __kmalloc_reserve(), "ip" argument
    hasn't been used. _RET_IP_ is embedded inside
    kmalloc_node_track_caller().
    Remove the redundant macro and rename the function after it.
    
    Signed-off-by: Alexander Lobakin <alobakin@pm.me>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -277,11 +277,8 @@ EXPORT_SYMBOL(netdev_alloc_frag);
  * may be used. Otherwise, the packet data may be discarded until enough
  * memory is free
  */
-#define kmalloc_reserve(size, gfp, node, pfmemalloc) \
-	 __kmalloc_reserve(size, gfp, node, _RET_IP_, pfmemalloc)
-
-static void *__kmalloc_reserve(size_t size, gfp_t flags, int node,
-			       unsigned long ip, bool *pfmemalloc)
+static void *kmalloc_reserve(size_t size, gfp_t flags, int node,
+			     bool *pfmemalloc)
 {
 	void *obj;
 	bool ret_pfmemalloc = false;
